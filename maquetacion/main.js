89e7bbb01e2e4c5a50f3fa2b8e05990aaeb2479f
Héctor Villalobos <h.villalobosm03@gmail.com>
2022-02-14 15:30:40 -0300

    jotform

--- maquetacion/main.js
+++ maquetacion/main.js
@@ -1,22 +1,22 @@
- const $form = document.querySelector('#form')
+//  const $form = document.querySelector('#form')
 
- $form.addEventListener('submit', handleSubmit)
+//  $form.addEventListener('submit', handleSubmit)
 
- async function handleSubmit(event) {
+//  async function handleSubmit(event) {
 
-     event.preventDefault()
-     const form = new FormData(this)
-     const response = await fetch(this.action,{
-         method: this.method,
-         body: form,
-         headers: {
-             'Accept': 'application/json'
-         }
-     })
+//      event.preventDefault()
+//      const form = new FormData(this)
+//      const response = await fetch(this.action,{
+//          method: this.method,
+//          body: form,
+//          headers: {
+//              'Accept': 'application/json'
+//          }
+//      })
 
-     if (response.ok) {
-         this.reset()
-         alert('Gracias por contactarte con nosostros')
-     }
+//      if (response.ok) {
+//          this.reset()
+//          alert('Gracias por contactarte con nosostros')
+//      }
 
- }
+//  }
